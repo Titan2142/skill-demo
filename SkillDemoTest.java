4c1055b3bd4bba4b6242a51b8b26958cea4e2168
Titan2142 <102689054+Titan2142@users.noreply.github.com>
2022-05-08 20:57:34 -0700

    adding a test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -1,9 +1,17 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
 
 public class SkillDemoTest {
     @Test
     public void addition() {
         assertEquals(2, SkillDemo.add(1, 1));
+    }
+
+    @Transient
+    public void add2() {
+        assertEquals("second test", 3, SkillDemo.add(1, 2));
     }
 }
